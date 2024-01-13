1e6498873213c6824854eb959906933d0130f971
Gelmut David <david-gelmut@rambler.ru>
2024-01-13 17:00:31 +0500

    Обновление сайта

---
+++ resources/js/views/product/Error404.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="product_cart_wrapper" style="height: 660px;">
+            <div class="cart_item" style="font-size: 24px;font-weight: bold;margin-top: 300px;margin-left: 300px;">
+                Страница не существует.Ошибка 404.Перейти на  <router-link to="/"><span style="text-decoration: underline;color:black; "> главную.</span></router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Error404"
+}
+</script>
+
