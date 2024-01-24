cb717b07b1d332a523b565034df72ad0d43bc8b8
Gelmut David <david-gelmut@rambler.ru>
2024-01-24 21:03:34 +0500

    Реализация отправки почты через очередь

---
+++ app/Jobs/SendMailToManagerJob.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\FeedbackMailer;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendMailToManagerJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    private $data;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Mail::to(env('MAIL_MANAGER'))->send(new FeedbackMailer($this->data));
+    }
+}
