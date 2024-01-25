023c55282139b11f0a58e91be1f7739b3c65dca3
Gelmut David <david-gelmut@rambler.ru>
2024-01-25 12:28:58 +0500

    Событие создания заказа , отправка на почту(Observer)

---
+++ app/Observers/OrderObserver.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Observers;
+
+use App\Mail\OrderMailer;
+use App\Models\Order;
+use Illuminate\Support\Facades\Mail;
+
+class OrderObserver
+{
+    /**
+     * Handle the Order "created" event.
+     */
+    public function created(Order $order): void
+    {
+        Mail::to(env('MAIL_MANAGER'))->send(new OrderMailer($order));
+    }
+
+    /**
+     * Handle the Order "updated" event.
+     */
+    public function updated(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "deleted" event.
+     */
+    public function deleted(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "restored" event.
+     */
+    public function restored(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "force deleted" event.
+     */
+    public function forceDeleted(Order $order): void
+    {
+        //
+    }
+}
